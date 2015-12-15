839878c5c56298e14b119b982a2726147acb78a5
Sean Tracey <seanmtracey@gmail.com>
2015-12-15 13:45:44 +0000

    Init

---
+++ noments.js
@@ -0,0 +1,5 @@
+var MomentsButton = document.querySelector('.moments.js-moments-tab');
+
+if(MomentsButton !== null){
+	MomentsButton.parentNode.removeChild(document.querySelector('.moments.js-moments-tab'));
+}
